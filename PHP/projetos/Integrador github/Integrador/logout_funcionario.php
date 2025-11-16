f5c5da83a46401c7f2a2e622993c91a54632d60b
Alex Kamizaki <alexiskusanagi@gmail.com>
2025-11-16 13:36:34 -0300

    fixing redundacies
    removing reduntant files

--- PHP/projetos/Integrador github/Integrador/logout_funcionario.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-
-session_destroy();
-
-    echo"<script>window.alert('Saindo da Aplicação'); </script>";
-    echo"<script>window.location.href='login_funcionario.php'; </script>";
-?>
