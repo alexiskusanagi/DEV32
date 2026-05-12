a98bae022febb6cfcc13edd78bdeaa4b2fb9041e
Alex Kamizaki <alexiskusanagi@gmail.com>
2026-05-12 13:05:04 -0300

    conversor binario
    conversor binario, decimal, hexadecimal

---
+++ C# Logica/Converter_bases_numericas_WindowsForms/Form1.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace Converter_bases_numericas_WindowsForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnConverter = new System.Windows.Forms.Button();
+            this.btnApagar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.clbOpcoes = new System.Windows.Forms.CheckedListBox();
+            this.txtBinario = new System.Windows.Forms.TextBox();
+            this.txtDecimal = new System.Windows.Forms.TextBox();
+            this.txtHexa = new System.Windows.Forms.TextBox();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnConverter
+            // 
+            this.btnConverter.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.btnConverter.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConverter.Location = new System.Drawing.Point(353, 79);
+            this.btnConverter.Name = "btnConverter";
+            this.btnConverter.Size = new System.Drawing.Size(97, 33);
+            this.btnConverter.TabIndex = 0;
+            this.btnConverter.Text = "Converter";
+            this.btnConverter.UseVisualStyleBackColor = false;
+            this.btnConverter.Click += new System.EventHandler(this.btnConverter_Click);
+            // 
+            // btnApagar
+            // 
+            this.btnApagar.BackColor = System.Drawing.Color.Yellow;
+            this.btnApagar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnApagar.Location = new System.Drawing.Point(480, 79);
+            this.btnApagar.Name = "btnApagar";
+            this.btnApagar.Size = new System.Drawing.Size(97, 33);
+            this.btnApagar.TabIndex = 1;
+            this.btnApagar.Text = "Limpar";
+            this.btnApagar.UseVisualStyleBackColor = false;
+            this.btnApagar.Click += new System.EventHandler(this.btnApagar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(214, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(268, 26);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Conversor Base Numérica";
+            // 
+            // clbOpcoes
+            // 
+            this.clbOpcoes.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.clbOpcoes.FormattingEnabled = true;
+            this.clbOpcoes.Items.AddRange(new object[] {
+            "Todos",
+            "Binário",
+            "Decimal",
+            "Hexadecimal"});
+            this.clbOpcoes.Location = new System.Drawing.Point(81, 160);
+            this.clbOpcoes.Name = "clbOpcoes";
+            this.clbOpcoes.Size = new System.Drawing.Size(120, 79);
+            this.clbOpcoes.TabIndex = 4;
+            // 
+            // txtBinario
+            // 
+            this.txtBinario.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBinario.Location = new System.Drawing.Point(23, 315);
+            this.txtBinario.Multiline = true;
+            this.txtBinario.Name = "txtBinario";
+            this.txtBinario.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtBinario.Size = new System.Drawing.Size(295, 137);
+            this.txtBinario.TabIndex = 5;
+            // 
+            // txtDecimal
+            // 
+            this.txtDecimal.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDecimal.Location = new System.Drawing.Point(324, 160);
+            this.txtDecimal.Multiline = true;
+            this.txtDecimal.Name = "txtDecimal";
+            this.txtDecimal.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtDecimal.Size = new System.Drawing.Size(273, 108);
+            this.txtDecimal.TabIndex = 6;
+            // 
+            // txtHexa
+            // 
+            this.txtHexa.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtHexa.Location = new System.Drawing.Point(324, 315);
+            this.txtHexa.Multiline = true;
+            this.txtHexa.Name = "txtHexa";
+            this.txtHexa.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtHexa.Size = new System.Drawing.Size(273, 137);
+            this.txtHexa.TabIndex = 7;
+            // 
+            // txtInput
+            // 
+            this.txtInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtInput.Location = new System.Drawing.Point(81, 89);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(243, 23);
+            this.txtInput.TabIndex = 8;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(424, 140);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 17);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Decimal";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(132, 295);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 17);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Binário";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(415, 295);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(88, 17);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Hexadecimal";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 545);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.txtHexa);
+            this.Controls.Add(this.txtDecimal);
+            this.Controls.Add(this.txtBinario);
+            this.Controls.Add(this.clbOpcoes);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnApagar);
+            this.Controls.Add(this.btnConverter);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConverter;
+        private System.Windows.Forms.Button btnApagar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckedListBox clbOpcoes;
+        private System.Windows.Forms.TextBox txtBinario;
+        private System.Windows.Forms.TextBox txtDecimal;
+        private System.Windows.Forms.TextBox txtHexa;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ C# Logica/Converter_bases_numericas_WindowsForms/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Converter_bases_numericas_WindowsForms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnConverter_Click(object sender, EventArgs e)
+        {
+            // 1. LIMPEZA DOS CAMPOS
+            txtBinario.Clear();
+            txtDecimal.Clear();
+            txtHexa.Clear();
+
+            if (string.IsNullOrWhiteSpace(txtInput.Text)) return;
+
+            string entrada = txtInput.Text.ToUpper();
+            long valorTotal = 0;
+
+            // 2. LER AS OPÇÕES DA LISTA (clbOpcoes)
+            bool querTodos = clbOpcoes.GetItemChecked(0);
+            bool querBin = clbOpcoes.GetItemChecked(1);
+            bool querDec = clbOpcoes.GetItemChecked(2);
+            bool querHex = clbOpcoes.GetItemChecked(3);
+
+            // 3. CÁLCULO DO VALOR DECIMAL ÚNICO (Base 36)
+            for (int i = 0; i < entrada.Length; i++)
+            {
+                char c = entrada[i];
+                int valorCaractere = char.IsDigit(c) ? c - '0' : c - 'A' + 10;
+                int expoente = entrada.Length - 1 - i;
+                valorTotal += valorCaractere * (long)Math.Pow(36, expoente);
+            }
+
+            // 4. BLOCO DECIMAL DIDÁTICO (Traduzindo letras para números no título)
+            if (querTodos || querDec)
+            {
+                string repNum = "";
+                foreach (char c in entrada)
+                {
+                    int v = char.IsDigit(c) ? c - '0' : c - 'A' + 10;
+                    repNum += v + " ";
+                }
+                txtDecimal.Text = $"SISTEMA DECIMAL (Base 36): {repNum.Trim()}\r\n";
+                txtDecimal.Text += "Fórmula Base: Σ (Dígito × 36^p)\r\n\r\n";
+                string aplicacao = "Aplicação: ";
+                for (int i = 0; i < entrada.Length; i++)
+                {
+                    int p = entrada.Length - 1 - i;
+                    int v = char.IsDigit(entrada[i]) ? entrada[i] - '0' : entrada[i] - 'A' + 10;
+                    aplicacao += $"({v} × 36^{p})";
+                    if (p > 0) aplicacao += " + ";
+                }
+                txtDecimal.Text += $"{aplicacao} = {valorTotal}";
+            }
+
+            // 5. BLOCO BINÁRIO DIDÁTICO
+            if (querTodos || querBin)
+            {
+                string b = Convert.ToString(valorTotal, 2);
+                txtBinario.Text = $"SISTEMA BINÁRIO: {b}\r\n";
+                txtBinario.Text += "Fórmula Base: Σ (bit × 2^p)\r\n\r\n";
+                string aplicacao = "Aplicação: ";
+                for (int i = 0; i < b.Length; i++)
+                {
+                    if (b[i] == '1')
+                    {
+                        int p = b.Length - 1 - i;
+                        aplicacao += $"2^{p} + ";
+                    }
+                }
+                txtBinario.Text += $"{aplicacao.TrimEnd(' ', '+')} = {valorTotal}";
+            }
+
+            // 6. BLOCO HEXADECIMAL DIDÁTICO
+            if (querTodos || querHex)
+            {
+                string h = valorTotal.ToString("X");
+                txtHexa.Text = $"SISTEMA HEXADECIMAL: {h}\r\n";
+                txtHexa.Text += "Fórmula Base: Σ (Dígito × 16^p)\r\n\r\n";
+                string aplicacao = "Aplicação: ";
+                for (int i = 0; i < h.Length; i++)
+                {
+                    int p = h.Length - 1 - i;
+                    int val = Convert.ToInt32(h[i].ToString(), 16);
+                    aplicacao += $"({val} × 16^{p})";
+                    if (p > 0) aplicacao += " + ";
+                }
+                txtHexa.Text += $"{aplicacao} = {valorTotal}";
+            }
+        }
+
+        private void btnApagar_Click(object sender, EventArgs e)
+        {
+            // Limpa o texto de entrada
+            txtInput.Clear();
+
+            // Limpa todos os campos de resultado e fórmula
+            txtBinario.Clear();
+            txtDecimal.Clear();
+            txtHexa.Clear();
+
+            // Desmarca todos os itens do CheckedListBox
+            for (int i = 0; i < clbOpcoes.Items.Count; i++)
+            {
+                clbOpcoes.SetItemChecked(i, false);
+            }
+
+            // Devolve o foco para o campo de entrada para o usuário digitar de novo
+            txtInput.Focus();
+        }
+    }
+}
